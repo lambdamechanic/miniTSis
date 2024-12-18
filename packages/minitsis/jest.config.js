bbc0a5805bda3b4f0dade191f44e14fe38f403d0
Mark Wotton <mwotton@gmail.com>
2024-12-18 10:22:51 +0700

    Revert "feat: Add code coverage configuration to Jest setup"
    This reverts commit 24687835dcdae837367a075ca576173ab739a144.

--- packages/minitsis/jest.config.js
+++ packages/minitsis/jest.config.js
@@ -2,21 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/build'],
-  collectCoverage: true,
-  coverageReporters: ['text', 'cobertura', 'lcov'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.test.ts',
-    '!src/**/*.spec.ts',
-    '!src/**/*.d.ts'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  }
 };
